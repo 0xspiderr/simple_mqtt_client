7353e28ae211e67c44af6335f1f44574a6a9c80e
0xspiderr <harypatrascu.1@gmail.com>
2026-03-26 19:28:36 +0200

    updated script to send json data

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+import json
+import time
 
 # variabile pentru stabilirea conexiunii
 port: int = 1883
@@ -17,5 +19,16 @@ client.connect(host, port)
 # subscribe la un topic
 client.subscribe(topic_name)
 
-# publishing-ul unui string pe topicul "test/test1" catre broker/server
-client.publish(topic_name, "hello world from mqtt_client.py")
+client.loop_start()
+# publishing-ul unui dictionar JSON pe topicul "test/test1" catre broker/server
+while True:
+	mesaj_input: str = input("introduce un mesaj:")
+	if mesaj_input in ["q", ""]:
+		print("iesire din program cu succes")
+		break
+	timestamp: float = time.time()
+	payload: dict = {"msg":mesaj_input, "timestamp":timestamp}
+
+	# publicarea dictionarului sub format json catre broker-ul mqtt
+	client.publish(topic_name, json.dumps(payload))
+client.loop_stop()
